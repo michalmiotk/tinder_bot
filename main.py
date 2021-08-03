6230e52023d45c010c4cbfcb3704b6101d6ab9da
Michał Miotk <michalmiotk95@gmail.com>
2021-08-04 00:30:23 +0200

    naprawiono problem z zbyt dlugim oczekiwaniem
    w momencie zamykania okna z cookie na fb login page, przegladarka uswiadamiala sobie ze jest automatycznie sterowana i dlatego sleep byl tutaj niepozadany

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from credentials import email, password
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class TinderBot():
     def __init__(self):
@@ -9,11 +14,29 @@ class TinderBot():
     def get_main_page(self):
         self.driver.get("https://tinder.com/")
 
+    def config_tinder_user_page(self):
+        allow_localisation_btn = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[3]/button[1]')
+        allow_localisation_btn.click()
+        allow_cookies_btn = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div/div[1]/button')
+        allow_cookies_btn.click()
+        not_allow_notification_about_likes_btn = self.driver.find_element_by_xpath(
+            '/html/body/div[2]/div/div/div/div/div[3]/button[2]/span')
+        not_allow_notification_about_likes_btn.click()
+
     def action_on_fb_login_page(self):
         self.driver.switch_to_window(self.driver.window_handles[1])
-        accept_cookie_btn = self.driver.find_element_by_xpath(
-            '/html/body/div[2]/div[2]/div/div/div/div/div[3]/button[2]')
+        time.sleep(3)
+        cookie_btn_xpath = '/html/body/div[2]/div[2]/div/div/div/div/div[3]/button[2]'
+        accept_cookie_btn = self.driver.find_element_by_xpath(cookie_btn_xpath)
         accept_cookie_btn.click()
+
+
+        while True:
+            try:
+                WebDriverWait(self.driver, 2).until(EC.presence_of_element_located((By.XPATH, cookie_btn_xpath)))
+            except TimeoutException:
+                print("waiting for cookie window dissapear")
+                break
         email_input = self.driver.find_element_by_xpath('/html/body/div/div[2]/div[1]/form/div/div[1]/div/input')
         email_input.send_keys(email)
         pass_input = self.driver.find_element_by_xpath('/html/body/div/div[2]/div[1]/form/div/div[2]/div/input')
@@ -29,24 +52,26 @@ class TinderBot():
         time.sleep(1)
         login_with_fb_xpath = '/html/body/div[2]/div/div/div[1]/div/div[3]/span/div[2]/button'
         login_with_fb_btn = self.driver.find_element_by_xpath(login_with_fb_xpath)
-        print(self.driver.window_handles)
         login_with_fb_btn.click()
         time.sleep(1)
-        print(self.driver.window_handles)
         self.action_on_fb_login_page()
+
         time.sleep(1)
-        print("hantl", self.driver.window_handles)
         self.driver.switch_to_window(self.driver.window_handles[0])
         time.sleep(5)
-        allow_localisation_btn = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[3]/button[1]')
-        allow_localisation_btn.click()
-        allow_cookies_btn = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div/div[1]/button')
-        allow_cookies_btn.click()
-        not_allow_notification_about_likes_btn = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[3]/button[2]/span')
-        not_allow_notification_about_likes_btn.click()
+        self.config_tinder_user_page()
         time.sleep(6)
-        swipe_right_btn = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]/button')
+        self.swipe_right_loop()
+
+    def swipe_right_once(self):
+        swipe_right_btn = self.driver.find_element_by_xpath(
+            '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]/button')
         swipe_right_btn.click()
+
+    def swipe_right_loop(self):
+        while True:
+            self.swipe_right_once()
+            time.sleep(2)
 
 if __name__ == '__main__':
     run = TinderBot()
--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
 # About
 Simple bot using selenium driver
+
+# Requirements
+install selenium chrome driver
+install python requirements
+```
+pip install -r requirements.txt
+```
+create credentials.py like
+email = '@gmail.pl'
+password = "somepass"
