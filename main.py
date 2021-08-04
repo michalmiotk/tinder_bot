b503dc85268163e9080b833689418a1f655dc2a8
Michał Miotk <michalmiotk95@gmail.com>
2021-08-04 10:09:49 +0200

    naprawiono problem z podwojnym oknem loginu fb

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from selenium import webdriver
-from credentials import email, password
 import time
+
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import selenium.common.exceptions
+
+from credentials import email, password
+
 
 class TinderBot():
     def __init__(self):
@@ -14,9 +18,10 @@ class TinderBot():
         self.driver = webdriver.Chrome(r'C:\chromedriver.exe', chrome_options=options)
 
         self.driver.maximize_window()
-
+        self.fb_cookie_btn_xpath = "//button[contains(@data-cookiebanner, 'accept_button')]"
     def get_main_page(self):
         self.driver.get("https://tinder.com/")
+        time.sleep(1)
 
     def config_tinder_user_page(self):
         allow_localisation_btn = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[3]/button[1]')
@@ -27,32 +32,40 @@ class TinderBot():
             '/html/body/div[2]/div/div/div/div/div[3]/button[2]/span')
         not_allow_notification_about_likes_btn.click()
 
-    def action_on_fb_login_page(self):
-        self.driver.switch_to_window(self.driver.window_handles[1])
-        time.sleep(3)
-        cookie_btn_xpath = '/html/body/div[2]/div[2]/div/div/div/div/div[3]/button[2]'
-        accept_cookie_btn = self.driver.find_element_by_xpath(cookie_btn_xpath)
-        accept_cookie_btn.click()
-
-
+    def wait_for_cookie_window_dissapear(self):
         while True:
             try:
-                WebDriverWait(self.driver, 1).until(EC.presence_of_element_located((By.XPATH, cookie_btn_xpath)))
+                WebDriverWait(self.driver, 1).until(EC.presence_of_element_located((By.XPATH, self.fb_cookie_btn_xpath)))
+                break
             except TimeoutException:
                 print("waiting for cookie window dissapear")
-                break
-        email_input = self.driver.find_element_by_xpath('/html/body/div/div[2]/div[1]/form/div/div[1]/div/input')
+    def handle_two_similar_fb_cookie_windows(self):
+        while not self.driver.find_element_by_xpath(self.fb_cookie_btn_xpath):
+            time.sleep(0.1)
+        accept_cookie_btn = self.driver.find_element_by_xpath(self.fb_cookie_btn_xpath)
+        accept_cookie_btn.click()
+        self.wait_for_cookie_window_dissapear()
+        time.sleep(6) #waiting to possible new appear same window
+        if self.driver.find_elements_by_xpath(self.fb_cookie_btn_xpath):
+            accept_cookie_btn = self.driver.find_element_by_xpath(self.fb_cookie_btn_xpath)
+            accept_cookie_btn.click()
+            self.wait_for_cookie_window_dissapear()
+
+
+    def action_on_fb_login_page(self):
+        self.driver.switch_to_window(self.driver.window_handles[1])
+        self.handle_two_similar_fb_cookie_windows()
+        email_input = self.driver.find_element_by_xpath('//input[contains(@name, "email")]')
         email_input.send_keys(email)
-        pass_input = self.driver.find_element_by_xpath('/html/body/div/div[2]/div[1]/form/div/div[2]/div/input')
+        pass_input = self.driver.find_element_by_xpath('//input[contains(@type, "password")]')
         pass_input.send_keys(password)
-        login_fb_btn = self.driver.find_element_by_xpath('/html/body/div/div[2]/div[1]/form/div/div[3]/label[2]/input')
+        login_fb_btn = self.driver.find_element_by_xpath('//button[contains(@name, "login")]')
         login_fb_btn.click()
 
     def login(self):
         login_btn_path = '//body/div/div/div/div/main/div/div/div/div/div/header/div/div[2]/div[2]'
         login_btn = self.driver.find_element_by_xpath(login_btn_path)
         login_btn.click()
-        import time
         time.sleep(1)
         login_with_fb_xpath = '/html/body/div[2]/div/div/div[1]/div/div[3]/span/div[2]/button'
         login_with_fb_btn = self.driver.find_element_by_xpath(login_with_fb_xpath)
@@ -62,17 +75,26 @@ class TinderBot():
 
         time.sleep(1)
         self.driver.switch_to_window(self.driver.window_handles[0])
-        time.sleep(5)
-        self.config_tinder_user_page()
-        time.sleep(8)
-        self.swipe_right_loop()
+
+    def click_heart_when_match(self):
+        #not implemented yet
+        any_btn = self.driver.find_element_by_xpath('//button/[contains')
+        any_btn.click()
 
     def swipe_right_once(self):
-        #like_xpath = '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]/button'
+        like_xpath = "//button[contains(@class, 'like-green')]"
+        try:
+            swipe_right_btn = self.driver.find_element_by_xpath(like_xpath)
+            swipe_right_btn.click()
+        except selenium.common.exceptions.ElementClickInterceptedException:
+            print("problem z kliknięciem - inny element otrzymał kliknięcie")
+            add_tinder_to_welcome_screen_xpath = "//button[contains(@data-testid, 'addToHomeScreen')]"
+            if self.driver.find_elements_by_xpath(add_tinder_to_welcome_screen_xpath):
+                add_tinder_to_welcome_screen_btn = self.driver.find_element_by_xpath(add_tinder_to_welcome_screen_xpath)
+                add_tinder_to_welcome_screen_btn.click()
+            else:
+                print("klikaj serce")
 
-        swipe_right_btn = self.driver.find_element_by_xpath("//button[contains(@class, 'like-green')]")
-
-        swipe_right_btn.click()
 
     def swipe_right_loop(self):
         while True:
@@ -84,3 +106,7 @@ if __name__ == '__main__':
     run = TinderBot()
     run.get_main_page()
     run.login()
+    time.sleep(5)
+    run.config_tinder_user_page()
+    time.sleep(8)
+    run.swipe_right_loop()
